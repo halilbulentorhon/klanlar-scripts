cdbea45159a17d0913805eebe48928f5655245bc
Halil Bülent Orhon <halil@192.168.1.7>
2024-11-09 20:06:29 +0300

    action env variable added

---
+++ common/constant.py
@@ -0,0 +1,2 @@
+action_env = "ACTION"
+scavenge_action = "scavenge"
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import os
+
+from common.constant import action_env, scavenge_action
 from common.util import load_config
 from scavenge.scavenge import execute_scavenge
 
 config = load_config('resources/config.yaml')
 
 if __name__ == '__main__':
-    execute_scavenge(config)
+    action = os.getenv(action_env)
+    if action == scavenge_action:
+        execute_scavenge(config)
+    else:
+        print("unknown action")

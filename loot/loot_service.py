e21f24ef0870335cff92adeaf2d13feab0c698c1
atadnmz <kadiratadonmez@gmail.com>
2024-11-24 10:54:02 +0100

    loss check added from reporting page

--- loot/loot_service.py
+++ loot/loot_service.py
@@ -1,10 +1,12 @@
 import math
 import time
 import requests
+import re
 from common.util import load_config
 from loot.extractor import initialize_loot_credentials, get_loot_ch_value
 from loot.loot_client import send_loot_request,get_close_vilages
-
+from common.util import get_base_request_headers
+from datetime import datetime
 
 def send_loot(config,targetId,target):
     loot_config = load_config('resources/loot_config.yaml')
@@ -95,13 +97,69 @@ def getBarbarianVillages():
 def add_minutes_to_millis(minutes):
     return int(time.time() * 1000) + (minutes * 60000)
 
+def convert_and_adjust(data):
+    result = []
+    seen_pairs = set()
+
+    for item in data:
+        # Extract x and y values
+        x, y = map(int, re.match(r'\((\d+)\|(\d+)\)', item).groups())
+        
+        # Adjust y if the pair already exists
+        while (x, y) in seen_pairs:
+            y -= 1  # Decrement y to make it unique
+        
+        # Add the unique (x, y) pair to the set and result list
+        seen_pairs.add((x, y))
+        result.append({"x": x, "y": y})
+
+    return result
+
+def loss_villages(config):
+    village_position = {
+            "x": 416,
+            "y": 570
+        }
+    loot_config = load_config('resources/loot_config.yaml')
+    for loot in loot_config:
+        village_id = loot["villageId"]
+
+    headers = get_base_request_headers(config, village_id, "report")
+    url = f"https://tr89.klanlar.org/game.php?screen=report&mode=all&from=0&village={village_id}"
+    response = requests.get(url, headers=headers)
+    pattern = r"\(\d+\|\d+\)"
+
+    matches = re.findall(pattern, response.text)
+    print(re.match(r'\((\d+)\|(\d+)\)', matches[0]).groups())
+    
+    
+    result = [{"x": int(x), "y": int(y)} for x, y in (re.match(r'\((\d+)\|(\d+)\)', item).groups() for item in matches)]
+
+    filtered_result = [entry for entry in result if entry != village_position]
+    return filtered_result
+
+def remove_matched_elements(data, pairs_to_remove):
+    remove_set = {(item["x"], item["y"]) for item in pairs_to_remove}
+    
+    filtered_data = [item for item in data if (item["x"], item["y"]) not in remove_set]
+    
+    return filtered_data
+
 
 def execute_loot(config):
-   
+
     barbar = getBarbarianVillages()
     new_barbar = [{**village, "finishTime": 0} for village in barbar]
+    
     while True:
-        for village in new_barbar:
+        loss = loss_villages(config)
+        print(loss)
+        current_time = datetime.now()
+        formatted_time = current_time.strftime("%H:%M:%S")
+        print("Current Time:", formatted_time)
+
+        updated_new_barbar = remove_matched_elements(new_barbar, loss)
+        for village in updated_new_barbar:
             if village["finishTime"] < int(time.time() * 1000):  # Check if finishTime is in the past
                 try:
                     # Make a POST request with the village's data
